b1f002e4ba1752316fc9d3286279840dcd68ee10
unknown <monkeyinastripedshirt@gmail.com>
2018-03-22 16:41:49 -0400

    Reformat and add more stuff"

--- src/Systems/LiftMechanism.cpp
+++ src/Systems/LiftMechanism.cpp
@@ -1,28 +1,20 @@
-#include <LiftMechanism.h>
+#include "LiftMechanism.h"
+#include <Spark.h>
 
-LiftMechanism::LiftMechanism() {
-	// TODO Auto-generated constructor stub
-
-}
-
-void LiftMechanism::SetIntake(int number) {
-	intake = new frc::Spark(number);
+LiftMechanism::LiftMechanism(int LiftID, int IntakeID) {
+    intake = new frc::Spark(IntakeID);
+    lift = new frc::Spark(LiftID);
 }
 
 void LiftMechanism::Intake(double speed) {
-	LiftMechanism::intake->Set(speed);
-}
-
-void LiftMechanism::SetLift(int number) {
-	lift = new frc::Spark(number);
+    LiftMechanism::intake->Set(speed);
 }
 
 void LiftMechanism::Lift(double speed) {
-	lift->Set(speed);
+    lift->Set(speed);
 }
 
 LiftMechanism::~LiftMechanism() {
-	// TODO Auto-generated destructor stub
-	delete intake;
+    // TODO Auto-generated destructor stub
 }
 
--- src/Systems/LiftMechanism.h
+++ src/Systems/LiftMechanism.h
@@ -1,19 +1,18 @@
 #pragma once
 
 #include <Spark.h>
+
 class LiftMechanism {
 public:
 
 
-	frc::Spark *intake;
-	frc::Spark *lift;
+    frc::Spark *intake;
+    frc::Spark *lift;
 
-	void SetIntake(int number);
-	void Intake(double speed);
+    void Intake(double speed);
 
-	void SetLift(int number);
-	void Lift(double speed);
+    void Lift(double speed);
 
-	LiftMechanism();
-	virtual ~LiftMechanism();
+    LiftMechanism(int LiftID, int IntakeID);
+    virtual ~LiftMechanism();
 };
